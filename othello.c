9314b1e9aa2d88d2cab8ecd57510c99b37e65a0d
g-hyoga <hyoga0216@gmail.com>
2017-12-30 18:18:23 +0900

    reverseできてない

--- othello.c
+++ othello.c
@@ -15,14 +15,16 @@ void getEnableCells(bool, Cell[HIGHT][WIDTH], bool[HIGHT][WIDTH]);
 bool existEnableCells(bool[WIDTH][HIGHT]);
 bool canPutLine(bool, int[2], int, Cell[HIGHT][WIDTH]);
 void add(int[2], int[2]);
-bool go(bool, int[2], int, Cell[HIGHT][WIDTH], bool);
+bool canPutLineIter(bool, int[2], int, Cell[HIGHT][WIDTH], bool);
 bool fillBoard(Cell[HIGHT][WIDTH]);
 void finishGame(Cell[HIGHT][WIDTH]);
 bool isOneColor(Cell[HIGHT][WIDTH]);
 void inputCell(int[2]);
 bool canPut(int[2], bool[WIDTH][HIGHT]);
 void reverse(bool, int[2], Cell[HIGHT][WIDTH]);
+void reverseIter(bool, int[2], int, Cell[WIDTH][HIGHT]);
 void displayBoard(Cell[HIGHT][WIDTH]);
+Cell getStoneColor(bool);
 
 int main() {
   // ボードの初期状態
@@ -46,6 +48,8 @@ int main() {
   if(isFirst) {
     isAI = false;
   }
+  printf("isFirst: %d\n",isFirst);
+  printf("isAI: %d\n",isAI);
 
   while(true) {
     bool enableCells[WIDTH][HIGHT];
@@ -74,20 +78,24 @@ int main() {
     int selectedCell[2] = {-1, -1};
     if(isAI){
       printf("AI phase\n");
-      //sugoiAlgorithm();
-      isAI = !isAI;
-      isFirst = !isFirst;
+      //sucanPutLineIteriAlcanPutLineIterrithm();
     } else {
       printf("human phase\n");
       inputCell(selectedCell);
       //printf("select: %d, %d\n", selectedCell[0], selectedCell[1]);
-      if (canPut(selectedCell, enableCells)) { // 置けない場所を選択していたらisAIフラグを変えずにもう一回ループしてもらう
+      if (!canPut(selectedCell, enableCells)) { // 置けない場所を選択していたらisAIフラグを変えずにもう一回ループしてもらう
         isAI = !isAI;
         isFirst = !isFirst;
+      } else {
+        printf("そこは置けません\n");
+        selectedCell[0] = -1;
+        selectedCell[1] = -1;
       }
     }
 
-    reverse(!isFirst, selectedCell, board);
+    reverse(isFirst, selectedCell, board);
+    isAI = !isAI;
+    isFirst = !isFirst;
     displayBoard(board);
   }
 
@@ -101,7 +109,7 @@ void getEnableCells(bool isFirst, Cell board[HIGHT][WIDTH], bool enableCells[WID
       int selectedCell[2] = {x, y};
       for(int i=0; i<8; i++) {
         if (canPutLine(isFirst, selectedCell, i, board)) {
-          printf("(%d, %d) ", x, y);
+          printf("(%d, %c) ", y+1, (x - 1) + 'a');
           enableCells[x][y] = true;
         }
       } 
@@ -110,10 +118,14 @@ void getEnableCells(bool isFirst, Cell board[HIGHT][WIDTH], bool enableCells[WID
 }
 
 bool canPutLine(bool isFirst, int selectedCell[2], int directionIndex, Cell board[HIGHT][WIDTH]) {
-  return go(isFirst, selectedCell, directionIndex, board, false);
+  return canPutLineIter(isFirst, selectedCell, directionIndex, board, false);
 }
 
-bool go(bool isFirst, int selectedCell[2], int directionIndex, Cell board[HIGHT][WIDTH],
+/*
+ *
+ * @param (flag) 名前が思いつかなかった. 隣の石が異なる色のときtrueにして呼び出す
+ */
+bool canPutLineIter(bool isFirst, int selectedCell[2], int directionIndex, Cell board[HIGHT][WIDTH],
     bool flag) {
   int directions[8][2] = {
     {-1,-1}, {0,-1}, {1,-1},
@@ -135,7 +147,7 @@ bool go(bool isFirst, int selectedCell[2], int directionIndex, Cell board[HIGHT]
 
   if (board[tmp[0]][tmp[1]] != isFirst && !flag) {
     // 置けているとき
-    return go(isFirst, tmp, directionIndex, board, true);
+    return canPutLineIter(isFirst, tmp, directionIndex, board, true);
   } else if (board[tmp[0]][tmp[1]] == isFirst && !flag) {
     // 横が同じ色だったとき 
     return false;
@@ -143,7 +155,7 @@ bool go(bool isFirst, int selectedCell[2], int directionIndex, Cell board[HIGHT]
     // 挟んだとき
     return true;
   }
-  return go(isFirst, tmp, directionIndex, board, false);
+  return canPutLineIter(isFirst, tmp, directionIndex, board, false);
 }
 
 void add(int target[2], int a[2]) {
@@ -232,7 +244,7 @@ void inputCell(int selectedCell[2]){
   // 入力後分割とキャスト
   
   char input[20];
-  printf(" 置く場所を入力\n e.g. 4,c\n input:");
+  printf(" 置く場所を入力\n input:");
   fgets(input,20,stdin);
   
   char *in;
@@ -251,45 +263,44 @@ bool canPut(int selectedCell[2], bool enableCells[WIDTH][HIGHT]) {
   return enableCells[selectedCell[0]][selectedCell[1]];
 }
 
-// nakagawa
 void reverse(bool isFirst, int selectedCell[2], Cell board[HIGHT][WIDTH]) {
   // selectedCellの場所に置き、boardを更新する
   int x = selectedCell[0], y = selectedCell[1];
-  board[x][y] = 0;
-  if(isFirst) {
-    board[x][y] = Black;
-  } else {
-    board[x][y] = White;
-  }
-  enum Cell cellcolor = board[x][y];
-  int nextcell[8][2] = { {-1,-1},{-1,0},{-1,1},
-    {0,-1},        {0,1},
-    {1,-1},{1,0},{1,1}};
-  for(int a = 0; a < 8; a++) {
-    int x2 = x+nextcell[a][0] , y2 = y+nextcell[a][1];
-    if(x2 < 0 || x2 > 7 || y2 < 0 || y2 > 7){
-      if(board[x2][y2] != Blank && board[x2][y2] != cellcolor) {
-        for(int i = 0; i < 6; i++) {
-          x2 += nextcell[a][0];
-          y2 += nextcell[a][1];
-          if(x2 < 0 || x2 > 7 || y2 < 0 || y2 > 7){
-            if(board[x2][y2] == Blank) {
-              break;
-            }else if(board[x2][y2] == cellcolor) {
-              for(int j = 0; j < i+1 ; j++) {
-                x2 -= nextcell[a][0];
-                y2 -= nextcell[a][1];
-                board[x2][y2] = cellcolor;
-              }
-            }
-          }
-        }
-      }
+  Cell myColor = getStoneColor(isFirst);
+
+  board[x][y] = myColor;
+  for (int i=0; i < 8; i++) {
+    if (canPutLine(isFirst, selectedCell, i, board)) {
+      reverseIter(isFirst, selectedCell, i, board);
     }
   }
 }
 
+void reverseIter(bool isFirst, int scanningCell[2], int directionIndex, Cell board[WIDTH][HIGHT]) {
+  int directions[8][2] = {
+    {-1,-1}, {0,-1}, {1,-1},
+    {-1, 0}, {1, 0},
+    {-1,1}, {0,1}, {1,1}
+  };
+  
+  int tmp[2] = {scanningCell[0], scanningCell[1]};
+  add(tmp, directions[directionIndex]);
+  
+  int x = tmp[0];
+  int y = tmp[1];
+
+  Cell myColor = getStoneColor(isFirst);
+
+  if (board[x][y] != myColor) {
+    return;
+  }
+
+  board[x][y] = myColor; 
+  reverseIter(isFirst, tmp, directionIndex, board);
+}
+
 void displayBoard(Cell board[HIGHT][WIDTH]) {
+  printf("--------------------------\n");
   printf("   ");
   for(int i = 0; i < WIDTH; i++){
     printf("%c ", 'a' + i);
@@ -301,7 +312,7 @@ void displayBoard(Cell board[HIGHT][WIDTH]) {
     for(int x=0; x < WIDTH ; x++) {
       switch (board[x][y]) {
         case Black:
-          printf("× ");
+          printf("● ");
           break;
         case White:
           printf("◯ ");
@@ -313,5 +324,11 @@ void displayBoard(Cell board[HIGHT][WIDTH]) {
     }
     printf("\n");
   }
-  printf("--------------------------\n");
+}
+
+Cell getStoneColor(bool isFirst) {
+  if(isFirst) {
+    return Black;
+  }
+  return White;
 }
